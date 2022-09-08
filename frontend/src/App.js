a48a1c80a1423e566d55dba48f9c4184f09cdb68
irmlin <irmantas.l75@gmail.com>
2022-09-08 16:18:40 +0300

    Added a drawing board component for drawing digits

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import CanvasDraw from "react-canvas-draw";
+import {useState, useRef} from "react";
+import {Button} from "@mui/material";
+import "./styles.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+
+const canvasRef = useRef(null)
+const [drawing, setDrawing] = useState(null)
+
+function updateDrawing() {
+  const base64 = canvasRef.current.canvasContainer.childNodes[1].toDataURL();
+  setDrawing(base64)
 }
 
-export default App;
+const onCanvasChange = (event) => {
+    updateDrawing()
+  };
+
+const onPredictButtonClick = (event) => {
+  console.log("predicting")
+}
+
+const onClearButtonClick = (event) => {
+  canvasRef.current.clear()
+  setDrawing(null)
+}
+
+const onUndoButtonClick = (event) => {
+  canvasRef.current.undo()
+  updateDrawing()
+}
+
+  return (
+    <div className={"content"}>
+      <CanvasDraw
+              ref={canvasRef}
+              style={{
+                boxShadow:
+                  "0 13px 27px -5px rgba(50, 50, 93, 0.25),    0 8px 16px -8px rgba(0, 0, 0, 0.3)"
+              }}
+              hideGrid={true}
+              brushRadius={10}
+              brushColor={"black"}
+              onChange={onCanvasChange}
+            />
+      <div style={{display:"flex"}}>
+        <Button style={{width:"33.3%"}} color={"success"} variant={"contained"} onClick={onPredictButtonClick}>
+          Predict
+        </Button>
+
+        <Button style={{width:"33.3%"}} color={"error"} variant={"contained"} onClick={onClearButtonClick}>
+          Clear Image
+        </Button>
+
+        <Button style={{width:"33.3%"}} variant={"contained"} onClick={onUndoButtonClick}>
+          Undo
+        </Button>
+      </div>
+    </div>
+  )
+}
