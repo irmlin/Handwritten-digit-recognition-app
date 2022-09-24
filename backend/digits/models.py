51f4ffa7cfa45db7934e768508a99b131626598d
irmlin <irmantas.l75@gmail.com>
2022-09-24 10:26:31 +0300

    new field for Digit model isTrainingDigit

--- backend/digits/models.py
+++ backend/digits/models.py
@@ -6,10 +6,12 @@ import numpy as np
 class Digit(models.Model):
     picture = NDArrayField(shape=(784, 1), dtype=np.float64)
     label = models.SmallIntegerField(null=True)
+    isTrainingDigit = models.BooleanField(default=True)
+
 
     @classmethod
-    def create(cls, picture, label):
-        digit = cls(picture=picture, label=label)
+    def create(cls, picture, label, isTrainingDigit):
+        digit = cls(picture=picture, label=label, isTrainingDigit=isTrainingDigit)
         return digit
 
     def __str__(self):
